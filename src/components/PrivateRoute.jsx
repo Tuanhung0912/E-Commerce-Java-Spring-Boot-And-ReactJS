3f8ac83e8b2bf0ab65d5aed09cef8a4af6187b6b
tuanhung0912 <tuanhung.bh2003@gmail.com>
2026-03-31 14:16:10 +0700

    Redirecting To Home After Login

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -5,9 +5,9 @@ import { Navigate, Outlet } from 'react-router-dom';
 const PrivateRoute = ({ publicPage = false }) => {
     const { user } = useSelector((state) => state.auth);
     if (publicPage) {
-        return user ? <Navigate to="/profile" replace /> : <Outlet />;
+       return user ? <Navigate to="/" /> : <Outlet />
     }
-    return user ? <Outlet /> : <Navigate to="/login" replace />;
+    return user ? <Outlet /> : <Navigate to="/login" />;
 }
 
 export default PrivateRoute
