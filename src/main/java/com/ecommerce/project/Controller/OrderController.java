38f66482ac38e3ac405d71cd154ed3e3c3b759b4
tuanhung0912 <tuanhung.bh2003@gmail.com>
2026-04-02 13:56:25 +0700

    Order's Data API

--- src/main/java/com/ecommerce/project/Controller/OrderController.java
+++ src/main/java/com/ecommerce/project/Controller/OrderController.java
@@ -1,7 +1,9 @@
 package com.ecommerce.project.Controller;
 
+import com.ecommerce.project.config.AppConstants;
 import com.ecommerce.project.payload.OrderDTO;
 import com.ecommerce.project.payload.OrderRequestDTO;
+import com.ecommerce.project.payload.OrderResponse;
 import com.ecommerce.project.payload.StripePaymentDTO;
 import com.ecommerce.project.service.OrderService;
 import com.ecommerce.project.service.StripeService;
@@ -53,6 +55,19 @@ public class OrderController {
         System.out.println("StripePaymentDTO Received " + stripePaymentDTO);
         PaymentIntent paymentIntent = stripeService.paymentIntent(stripePaymentDTO);
         return new ResponseEntity<>(paymentIntent.getClientSecret(), HttpStatus.CREATED);
+    }
+
+    @Tag(name = "Order APIs", description = "APIs for managing orders")
+    @Operation(summary = "Order Data (admin)", description = "API to view Order Data")
+    @GetMapping("/admin/orders")
+    public ResponseEntity<OrderResponse> getAllOrders(
+            @RequestParam(name = "pageNumber", defaultValue = AppConstants.PAGE_NUMBER, required = false) Integer pageNumber,
+            @RequestParam(name = "pageSize", defaultValue = AppConstants.PAGE_SIZE, required = false) Integer pageSize,
+            @RequestParam(name = "sortBy", defaultValue = AppConstants.SORT_ORDERS_BY, required = false) String sortBy,
+            @RequestParam(name = "sortOrder", defaultValue = AppConstants.SORT_DIR, required = false) String sortOrder
+    ) {
+        OrderResponse orderResponse = orderService.getAllOrders(pageNumber, pageSize, sortBy, sortOrder);
+        return new ResponseEntity<OrderResponse>(orderResponse, HttpStatus.OK);
     }
 }
 
